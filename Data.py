2362f4a61e4350d95e0c0ba05d6303bd450f075a
Sarthak <sarthak2019fractal@gmail.com>
2025-02-01 23:27:47 +0530

    Inital Commit

---
+++ Data.py
@@ -0,0 +1,19 @@
+# # List of Frontier Models
+
+# 1-OpenAi
+# Model-> GPT
+
+# 2-Antropic
+# Model-> Claude
+
+# 3-Google
+# Model-> Gemini
+
+# 4-Cohere
+# Model->Command R+
+
+# 5-Meta
+# Model-> Llama
+
+# 6-Perplexity
+# Model->Perplexity
